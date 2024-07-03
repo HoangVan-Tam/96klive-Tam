a097aa51982c603eaad40837580ec892c554b649
tam.hoang@smsdome.com <tam.hoang@smsdome.com>
2024-07-03 19:40:00 +0700

    updated

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Data_Carousel } from './common/Contants'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold underline h-screen bg-cyan-500">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline h-screen bg-cyan-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/common/Contants.tsx
@@ -0,0 +1,5 @@
+export const Data_Carousel = [
+    `"96K Live is established on the premise that the management are passionate about what we do and where we have come from. We want to enjoy working in what we believe to be a special industry and protect that which makes it different to other working environments."`,
+    `"We want to look after our clients to the best of our ability, encourage a respectful and fun working environment, and we hope this will mean we can offer service and support to many of the next generation of touring productions"`,
+    `"We are very excited by what the future holds. We believe that actions speak louder than words and so look forward to demonstrating to the market our capability as we take on the exciting opportunities that 2024 has to offer."`
+]
